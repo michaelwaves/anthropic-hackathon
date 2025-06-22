f22861e0580696158903b5ae0c974c1e2f8a9786
michaelwaves <michaelyu713705@gmail.com>
2025-06-22 18:06:20 -0400

    add hiring

--- frontend/components/navigation/CountrySelector.tsx
+++ frontend/components/navigation/CountrySelector.tsx
@@ -67,7 +67,7 @@ const CountrySelector: React.FC = () => {
                     disabled={!country1 || !country2}
                     className="px-6 py-2 bg-blue-600 text-white rounded-md font-medium hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
                 >
-                    Generate Comparison Data
+                    View Data
                 </button>
             </div>
         </div>
